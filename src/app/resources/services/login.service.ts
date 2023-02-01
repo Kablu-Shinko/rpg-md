4074fce3a9c6e04925372925860b9d0d4ff6b376
Deric <dericprado@gmail.com>
2023-02-01 20:00:44 -0300

    Telinha de Login, quase lá

---
+++ src/app/resources/services/login.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RequestLogin } from '../models/RequestLogin';
+import { ResponseLogin } from '../models/ResponseLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public doLogin(requestLogin: RequestLogin) : Observable<ResponseLogin> {
+    return this.httpClient.get<ResponseLogin>('https://localhost:7094/rpgmd/login');
+
+  }
+}
